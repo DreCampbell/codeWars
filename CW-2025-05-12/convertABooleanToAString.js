20952c457984b12cbf4beee0bc04d3b20554ea4e
DreCampbell <drecampbell.dev@gmail.com>
2025-05-12 15:44:57 +0800

    Coding challenge and solution for 2025-05-12

---
+++ CW-2025-05-12/convertABooleanToAString.js
@@ -0,0 +1,12 @@
+/*
+8 Kyu
+Convert a Boolean to a String!
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+function booleanToString(b){
+    return String(b);
+}
