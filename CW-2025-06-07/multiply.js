74d79ae7b3b8398cba608c4881923b61234f532c
DreCampbell <drecampbell.dev@gmail.com>
2025-06-07 10:25:23 +0800

    Coding challenge and solution for 2025-06-07

---
+++ CW-2025-06-07/multiply.js
@@ -0,0 +1,16 @@
+/*
+8 Kyu
+Multiply
+
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+function is missing the return keyword
+*/
+
+function multiply(a, b){
+  return a * b;
+}
