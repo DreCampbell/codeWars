c70e4c8a586a96a1e1aeba9ec94b7db2679f8309
DreCampbell <drecampbell.dev@gmail.com>
2025-06-10 17:59:49 +0800

    Coding solution

---
+++ CW-2025-06-10/incorrectDivisionMethod.js
@@ -0,0 +1,19 @@
+/*
+8 Kyu
+Incorrect Division Method
+
+Description:
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+const solve = (x, y) => x // y
+
+//PARAMETERS: 
+
+//RETURNS: 
+
+//EXAMPLES: 
+
+//PSEUDOCODE: 
+*/
+
+const solve = (x, y) => x / y;
