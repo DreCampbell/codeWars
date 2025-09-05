ea42c440b89742e062f0c9de3b15f089b354941c
DreCampbell <drecampbell.dev@gmail.com>
2025-09-05 19:38:10 +0800

    Coding challenge and solution for 2025-09-03

---
+++ CW-2025-09-03/paddedNumbers.js
@@ -0,0 +1,21 @@
+/*
+7 Kyu
+Substituting Variables Into Strings: Padded Numbers
+
+Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+Example:
+
+solution(5); // should return "Value is 00005"
+
+*/
+
+function solution(value){
+    let zeros = '0';
+    return `Value is ${zeros.repeat(5 - value.toString().length)}${value}`;
+}
+
+console.log(solution(5),"Value is 00005")
+console.log(solution(1204),"Value is 01204")
+console.log(solution(109),"Value is 00109")
+console.log(solution(0),"Value is 00000")
