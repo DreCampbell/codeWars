c7d30dc900d4bf2fc4fe0b5a44b8fc8929fb6636
DreCampbell <drecampbell.dev@gmail.com>
2026-01-08 17:30:23 +0800

    Coding challenge and solution for 2026-01-08

---
+++ CW-2026-01-08/sumOfMinimums.js
@@ -0,0 +1,35 @@
+/*
+7 Kyu
+Sum of Minimums
+
+Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+For Example:
+
+[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+]
+So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+Note: You will always be given a non-empty list containing positive values.
+
+ENJOY CODING :)
+
+//PARAMETERS: a nested array with arrays of numbers
+
+//RETURNS: return the sum of the smallest number in each of the arrays
+
+//EXAMPLES: 
+    (sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]), 9);
+    (sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]), 76);
+
+//PSEUDOCODE: iterate through the array retuning the smallest number with Math.min(), then use the reduce method to get the sum
+*/
+
+function sumOfMinimums(arr){
+    return arr.map(x => Math.min(...x)).reduce((a, b) => a + b, 0);
+}
+
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]), 9);
+console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]), 76);
