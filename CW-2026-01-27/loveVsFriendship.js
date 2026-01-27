4b2efe6e02f2d6852cb97321e8b82b9682d21d2e
DreCampbell <drecampbell.dev@gmail.com>
2026-01-27 22:24:16 +0800

    Coding challenge and solution for 2026-01-27

--- CW-2026-01-27/loveVsFriendship.js
+++ CW-2026-01-27/loveVsFriendship.js
@@ -37,4 +37,4 @@ console.log(wordsToMarks("attitude"), 100);
 console.log(wordsToMarks("friends"), 75);
 console.log(wordsToMarks("family"), 66);
 console.log(wordsToMarks("selfness"), 99);
-console.log(wordsToMarks("knowledge"), 96);
+console.log(wordsToMarks("knowledge"), 96); 
