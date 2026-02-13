6b98adbc9f2380d88a1c6c83a6818b853fae466c
DreCampbell <drecampbell.dev@gmail.com>
2026-02-14 05:14:51 +0800

    Coding challenge and solution for 2026-02-14

---
+++ CW-2026-02-14/sortArrays-1.js
@@ -0,0 +1,19 @@
+/*
+7 Kyu
+Sort Arrays - 1
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+//PARAMETERSL: 
+
+//RETURNS: 
+
+//EXAMPLES: 
+
+//PSEUDOCODE: 
+*/
+
+function sortme(names){
+    return names.sort();
+}
+
