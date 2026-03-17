952e0cbe45026bfe67bdfe26e58bcd8aff63c721
DreCampbell <drecampbell.dev@gmail.com>
2026-03-17 19:42:11 +0800

    Coding challenge and solution for 2026-03-17

---
+++ CW-2026-03-17/alphabeticallyOrdered.js
@@ -0,0 +1,23 @@
+/*
+7 Kyu
+Alphabetically Ordered
+
+Your task is very simple. Just write a function that takes an input string of lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+
+Examples (input -> output)
+"kata" -> false ('a' comes after 'k')
+"ant" -> true (all characters are in alphabetical order)
+Good luck :)
+
+*/
+
+function alphabetic(s) {
+  return s === s.split("").sort().join("");
+}
+
+console.log(alphabetic("asd"), false);
+console.log(alphabetic("codewars"), false);
+console.log(alphabetic("door"), true);
+console.log(alphabetic("cell"), true);
+console.log(alphabetic("z"), true);
+console.log(alphabetic(""), true);
