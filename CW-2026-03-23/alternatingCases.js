d722c0ad23675a65941ed4c7d2e5b4220fdd60e4
DreCampbell <drecampbell.dev@gmail.com>
2026-03-24 17:41:16 +0800

    Coding challenge and solution for 2026-03-23

---
+++ CW-2026-03-23/alternatingCases.js
@@ -0,0 +1,19 @@
+/*
+8 Kyu
+Alternating Cases
+
+Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+
+*/
+
+function alternateCase(s) {
+  return s
+    .split("")
+    .map((x) => (x !== x.toLowerCase() ? x.toLowerCase() : x.toUpperCase()))
+    .join("");
+}
+
+console.log(alternateCase("abc"), "ABC");
+console.log(alternateCase("ABC"), "abc");
+console.log(alternateCase("Hello World"), "hELLO wORLD");
