e8ffdd5f6b657febb312e17b340f4935fc7cdcfb
DreCampbell <drecampbell.dev@gmail.com>
2026-06-03 06:53:30 +0800

    Coding challenge and solution for 2026-06-03

---
+++ CW-2026-06-03/basicDataTypesString.js
@@ -0,0 +1,41 @@
+/*
+8 Kyu
+Training JS #3: Basic Data Types - String
+
+In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., various symbol: @#$%^&*, control symbol: \n,\t etc..
+
+String can use operator + to connect two or more strings together.
+
+Task
+Misson 1:
+I've created three functions, and defined some global variables, please select some variables that can make up the name of the function, and return them (please note the uppercase and lowercase letters are different).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+Misson 2:
+After misson 1 finished, you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2, answer3
+
+If everything is right, click "Submit" again to submit your code pass this kata.
+*/
+
+((a1 = "A"),
+  (a2 = "a"),
+  (b1 = "B"),
+  (b2 = "b"),
+  (c1 = "C"),
+  (c2 = "c"),
+  (d1 = "D"),
+  (d2 = "d"),
+  (e1 = "E"),
+  (e2 = "e"),
+  (n1 = "N"),
+  (n2 = "n"));
+
+const Dad = () => d1 + a2 + d2;
+const Bee = () => b1 + e2 + e2;
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2;
+
+//answer some questions if you finished works above
+const answer1 = () => "no";
+const answer2 = () => "no";
+const answer3 = () => "yes";
